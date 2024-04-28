71b78af2b1a272eec269967e89ab0dc7e31a2d81
beninseattle <bena@mac.com>
2024-04-28 03:39:19 +0000

    Section 5 form validation and error handling

--- snippets/src/actions/index.ts
+++ snippets/src/actions/index.ts
@@ -4,11 +4,47 @@ import { redirect } from "next/navigation";
 import { db } from "@/db";
 
 export async function editSnippet(id: number, code: string) {
-    await db.snippet.update({ where: { id }, data: { code }});
+    await db.snippet.update({ where: { id }, data: { code } });
     redirect(`/snippets/${id}`);
 };
 
 export async function deleteSnippet(id: number) {
-    await db.snippet.delete({ where: { id }});
+    await db.snippet.delete({ where: { id } });
     redirect(`/`);
+}
+
+export async function createSnippet(formState: { message: string }, formData: FormData) {
+    try {
+        const title = formData.get('title');
+        const code = formData.get('code');
+
+        if (typeof title !== 'string' || title.length < 3) {
+            return {
+                message: 'Title must be longer'
+            };
+        }
+        if (typeof code !== 'string' || code.length < 10) {
+            return {
+                message: 'Code must be longer'
+            };
+        }
+
+        await db.snippet.create({
+            data: {
+                title,
+                code
+            }
+        });
+    } catch (error) {
+        if (error instanceof Error) {
+            return {
+                message: 'An error occurred: ' + error.message
+            };
+        }
+        return {
+            message: 'An unknown error occurred'
+        };
+    }
+
+    redirect('/');
 }
